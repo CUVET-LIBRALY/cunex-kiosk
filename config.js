2d1fb98e34e8750b2066c1f4d140bef46706e0f5
CUVET-LIBRALY <hathaichanok097@gmail.com>
2026-09-07 16:59:56 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,14 @@
+window.DASHBOARD_CONFIG = {
+  // ข้อความหัวเรื่องด้านบน
+  headerTitle: "ตารางสถานะห้องประชุม (ห้องสมุดฯ อาคาร 60 ปี คณะสัตวแพทยศาสตร์ จุฬาฯ)",
+  
+  // ข้อความใต้ QR Code
+  qrTitle: "สแกนเพื่อจองห้อง",
+  qrSubtitle: "CU NEX Backoffice",
+  
+  // คำอธิบายสถานะ (Legend)
+  legendFree: "ว่าง (Free)",
+  legendPending: "รออนุมัติ / สนใจ",
+  legendBusy: "ไม่ว่าง / ถูกจอง",
+  legendClosed: "ปิดบริการ"
+};
